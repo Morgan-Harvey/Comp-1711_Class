0f0b8e960c38c12bfe175d3fcfebe72b9fe8b632
Morgan-Harvey <146832750+Morgan-Harvey@users.noreply.github.com>
2023-10-30 16:55:47 +0000

    Assessment_project

---
+++ assessments/assessment_1/StepsTasks.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Define any additional variables here
+ char filerecords [] = "./FitnessData_2023.csv";
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
+
+// Complete the main function
+int main() {
+
+FILE *file;
+file = fopen("filerecords","r");
+
+if (file == NULL)
+{
+    perror("File not detected, please try again");
+    return 1;
+}
+
+int buf_size = 1024;
+char lin_buf[buf_size];
+while (fgets(lin_buf, buf_size, file))
+{
+    printf("%s", lin_buf);
+}
+
+
+}
