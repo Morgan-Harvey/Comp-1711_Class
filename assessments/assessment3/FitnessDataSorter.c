dc49205ecdb8f489afa333271a2a7897f93525f3
Morgan-Harvey <146832750+Morgan-Harvey@users.noreply.github.com>
2023-12-05 16:25:48 +0000

    Bug_fixing

--- assessments/assessment3/FitnessDataSorter.c
+++ assessments/assessment3/FitnessDataSorter.c
@@ -56,13 +56,14 @@ int main() {
     int stepsint = 0;
 
     count = 0;
+    // asks user to input filename
         printf("Input file: ");
 	    scanf("%s", fname);
         while (getchar() != '\n');
         
         strcat(filename, fname);
 
-        
+        // checks for filename and if it doesnt exist print error message and exit out of program
         FILE *filefrom;
 
         filefrom = fopen(filename, "r");
@@ -73,7 +74,7 @@ int main() {
             
         }
 
-
+        // tokenises the data and stores into the array 
         while (fgets(lin_buf, buf_size, filefrom)){
             tokeniseRecord(lin_buf, ',', date, time, &stepsint);
 
@@ -85,17 +86,19 @@ int main() {
 
         }
     fclose(filefrom);
+    //loops through the struct and will exit the program if conditons arent met
     for (int i = 0; i < count; i++)
     {
-        if (strlen(recorddata[i].date) != 10 || strlen(recorddata[i].time) != 5 || recorddata[i].steps < 0 ) 
+        if (strlen(recorddata[i].date) != 10 || strlen(recorddata[i].time) != 5 || recorddata[i].steps <= 0) 
         {
             printf("Error: invalid file\n");
             return 1;
         } 
     }
-    
+    // will sort the data in descending order when conditions arent emt
     qsort(recorddata,count, sizeof(FitnessData), compar);
     
+    //creates and writes the sorted data into a file
     FILE *fileto;
     strcat(filename,".tsv");
     fileto = fopen(filename, "w");
---
+++ assessments/assessment3/atoi.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+int steps = atoi("");
+printf("%d\n",steps);
+}
